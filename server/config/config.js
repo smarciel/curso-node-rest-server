999b878a94c824571b3698762c91467dc13f334c
smarciel <sarah.marciel@gmail.com>
2020-04-13 11:47:28 +0200

    mongo env variable

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://smarciel:cqYkYGivJTymOx1j@cluster0-g8r8e.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
